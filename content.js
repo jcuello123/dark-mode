7ffd50792af113fc1414c5d7b18f4e8c887f9418
jcuello123 <jcuello123@yahoo.com>
2020-07-30 18:31:21 -0400

    Added more variety. Refactored methods: Methods now only run when a different color is requested

--- content.js
+++ content.js
@@ -1,77 +1,79 @@
+const body = document.querySelector("body");
 const bgcolor = localStorage.getItem("background");
 const fontcolor = localStorage.getItem("fontcolor");
-const body = document.querySelector("body");
-const bodies = document.getElementsByTagName("body");
-const divs = document.getElementsByTagName("div");
-const paragraphs = document.getElementsByTagName("p");
-const inputs = document.getElementsByTagName("input");
-const as = document.getElementsByTagName("a");
-const h1s = document.getElementsByTagName("h1");
-const h2s = document.getElementsByTagName("h2");
-const h3s = document.getElementsByTagName("h3");
-const h4s = document.getElementsByTagName("h4");
-const h5s = document.getElementsByTagName("h5");
-const h6s = document.getElementsByTagName("h6");
+
+const elements = {
+  bodies: document.getElementsByTagName("body"),
+  divs: document.getElementsByTagName("div"),
+  spans: document.getElementsByTagName("span"),
+  paragraphs: document.getElementsByTagName("p"),
+  inputs: document.getElementsByTagName("input"),
+  tds: document.getElementsByTagName("td"),
+  as: document.getElementsByTagName("a"),
+  h1s: document.getElementsByTagName("h1"),
+  h2s: document.getElementsByTagName("h2"),
+  h3s: document.getElementsByTagName("h3"),
+  h4s: document.getElementsByTagName("h4"),
+  h5s: document.getElementsByTagName("h5"),
+  h6s: document.getElementsByTagName("h6"),
+  ems: document.getElementsByTagName("em"),
+  footers: document.getElementsByTagName("footer"),
+};
 
 //on page load
 if (bgcolor) {
   body.style.background = bgcolor;
-  changeBackgroundColor(bodies, divs, inputs);
+  changeBackgroundColor(elements);
 }
-if (fontcolor)
-  changeFontColor(
-    bodies,
-    divs,
-    inputs,
-    paragraphs,
-    h1s,
-    h2s,
-    h3s,
-    h4s,
-    h5s,
-    h6s,
-    as
-  );
+if (fontcolor) changeFontColor(elements);
 
 //when user clicks a color
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.background) {
-    localStorage.setItem("background", request.background);
-    changeBackgroundColor(bodies, divs, inputs);
+  const background = request.background;
+  const fontcolor = request.fontcolor;
+
+  if (background) {
+    console.log("in bg");
+    if (localStorage.getItem("background") !== background) {
+      localStorage.setItem("background", background);
+      changeBackgroundColor(elements);
+    }
   }
 
-  if (request.fontcolor) {
-    localStorage.setItem("fontcolor", request.fontcolor);
-    changeFontColor(
-      bodies,
-      divs,
-      inputs,
-      paragraphs,
-      h1s,
-      h2s,
-      h3s,
-      h4s,
-      h5s,
-      h6s,
-      as
-    );
+  if (fontcolor) {
+    if (localStorage.getItem("fontcolor") !== fontcolor) {
+      localStorage.setItem("fontcolor", fontcolor);
+      changeFontColor(elements);
+    }
+  }
+
+  if (request.clear) {
+    if (localStorage.getItem("background") !== "") {
+      localStorage.setItem("background", "");
+      changeBackgroundColor(elements);
+    }
+    if (localStorage.getItem("fontcolor") !== "") {
+      localStorage.setItem("fontcolor", "");
+      changeFontColor(elements);
+    }
   }
 });
 
-function changeBackgroundColor() {
+function changeBackgroundColor(allElements) {
   const color = localStorage.getItem("background");
-  for (elements of arguments) {
-    for (element of elements) {
-      element.style.background = color;
+
+  for (elmts in allElements) {
+    for (let i = 0; i < allElements[elmts].length; i++) {
+      allElements[elmts][i].style.background = color;
     }
   }
 }
 
-function changeFontColor() {
+function changeFontColor(allElements) {
   const color = localStorage.getItem("fontcolor");
-  for (elements of arguments) {
-    for (element of elements) {
-      element.style.color = color;
+  for (elmts in allElements) {
+    for (let i = 0; i < allElements[elmts].length; i++) {
+      allElements[elmts][i].style.color = color;
     }
   }
 }
--- handler.js
+++ handler.js
@@ -1,6 +1,28 @@
-const backgrounds = ["dark-blue", "dark-purple", "dark-green", "dark-gray"];
+const backgrounds = [
+  "dark-blue",
+  "dark-purple",
+  "rose",
+  "dark-gray",
+  "black",
+  "marsala",
+  "steelblue",
+  "teal",
+  "coral",
+  "brown",
+];
 
-const fonts = ["antiquewhite", "lavender", "cyan", "light-green"];
+const fonts = [
+  "antiquewhite",
+  "pink",
+  "cyan",
+  "light-green",
+  "white",
+  "rosybrown",
+  "dark-brown",
+  "peru",
+  "goldenrod",
+  "midnightblue",
+];
 
 for (id of backgrounds) {
   const btn = document.getElementById(id);
@@ -11,6 +33,8 @@ for (id of fonts) {
   const btn = document.getElementById(id);
   btn.addEventListener("click", () => font(btn.style.background));
 }
+
+document.getElementById("clear").addEventListener("click", clear);
 
 function background(bgcolor) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -24,6 +48,14 @@ function font(fontcolor) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { fontcolor: fontcolor }, () =>
       console.log("font color sent")
+    );
+  });
+}
+
+function clear() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { clear: "clear" }, () =>
+      console.log("clear request sent")
     );
   });
 }
